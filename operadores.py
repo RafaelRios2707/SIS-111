6d4c665a8784bae763ad8a6b4817e7e2fb8bd2f2
RafaelRios2707 <sebastian.rios@ucb.edu.bo>
2025-04-10 09:48:12 -0400

    Add files via upload

---
+++ operadores.py
@@ -0,0 +1,33 @@
+ #Crear variable
+saludo= "¡¡¡Hola "
+print(saludo)
+
+
+#SUMA
+n1=10
+n2=20
+suma=(n1+n2)
+print(suma) 
+
+n1="Palo"
+n2="manga!!!"
+suma_p=(n1+n2)
+print(saludo+suma_p) 
+
+#Resta
+n1=10
+n2=20
+resta=(n1-n2)
+print(resta) 
+
+#Multiplicacion
+n1=10
+n2=20
+mult=(n1*n2)
+print(mult)
+
+#Division
+n1=10
+n2=20
+div=(n1/n2)
+print(div) 
