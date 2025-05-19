2832ceb32274fa0d3935a5b277c461adb490c2f7
RafaelRios2707 <sebastian.rios@ucb.edu.bo>
2025-05-19 00:29:58 -0400

    Update prueba.py

--- PROGRAMACION_BASICA/prueba.py
+++ PROGRAMACION_BASICA/prueba.py
@@ -4,14 +4,16 @@ suma_par=0
 suma_impar=0
 while (numero>0):
     numero%10
-    numero=numero//10
-    if (numero%2==0):
+    if numero==0:
+        suma_par=suma_par
+        suma_impar=suma_impar     
+    elif (numero%2==0):
         suma_par=suma_par+1
     else:
         suma_impar=suma_impar+1
+    numero=numero//10
 
-print(suma_par,suma_impar)
-
+print(suma_par,"son pares y",suma_impar, "son impares (el 0 cuenta como par)")
 
 
 
